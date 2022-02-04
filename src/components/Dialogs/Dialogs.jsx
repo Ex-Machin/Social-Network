6a7fce31a0a04c68c5c75694857f7f48830d15ec
Dmitry Mikhailyuk <grinpigrin@gmail.com>
2022-02-04 11:27:38 +0000

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-  <div>
-      Dialogs
+  <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Lisa
+        </div>
+        <div className={s.dialog}>
+          Vika
+        </div>
+        <div className={s.dialog}>
+          Lilia
+        </div>
+        <div className={s.dialog}>
+          Lisa
+        </div>
+        <div className={s.dialog}>
+          Lisa
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>What da fuck!</div>
+        <div className={s.message}>Esketit</div>
+      </div>
   </div>
   );
 };
