ea7c9127ff52d04a85871b8e2e0e9a8c72429f82
Ex-Machin <grinpigrin@gmail.com>
2022-06-05 12:00:38 +0200

    copy of state in dialogReducer

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -17,15 +17,16 @@ const profileReducer = (state = initialState, action) => {
         message: state.newPostText,
         likesCoint: 0,
       };
-      let stateCopy = {...state}
-      stateCopy.posts = [...state.posts];
-      stateCopy.posts.push(newPost);
-      stateCopy.newPostText = "";
-      return stateCopy;
-    case UPDATE_NEW_POST_TEXT:{
-      let stateCopy = {...state};
-      stateCopy.newPostText = action.newText;
-      return stateCopy;
+      return {
+        ...state,
+        posts: [...state.posts, newPost],
+        newPostText: "",
+      };
+    case UPDATE_NEW_POST_TEXT: {
+      return {
+        ...state,
+        newPostText: action.newText,
+      };
     }
     default:
       return state;
