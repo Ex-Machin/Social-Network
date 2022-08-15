965538d81d8f6e81d586fb965ec5d523971acf2e
Ex-Machin <grinpigrin@gmail.com>
2022-08-15 10:32:28 +0200

    pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,25 +1,39 @@
 import { connect } from "react-redux";
-import { followActionCreator, setUsersActionCreator, unfollowActionCreator } from "../../redux/usersReducer";
+import {
+  followActionCreator,
+  setUsersActionCreator,
+  unfollowActionCreator,
+  setCurrentPageAC,
+  setUsersTotalCountAC,
+} from "../../redux/usersReducer";
 import Users from "./Users";
 
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
-}
+};
 const mapDispatchToProps = (dispatch) => {
   return {
-      follow: (userId) => {
-        dispatch(followActionCreator(userId))
-      },
-      unfollow: (userId) => {
-        dispatch(unfollowActionCreator(userId))
-      },
-      setUsers: (users) => {
-        dispatch(setUsersActionCreator(users))
-      }
-  }
-}
-
+    follow: (userId) => {
+      dispatch(followActionCreator(userId));
+    },
+    unfollow: (userId) => {
+      dispatch(unfollowActionCreator(userId));
+    },
+    setUsers: (users) => {
+      dispatch(setUsersActionCreator(users));
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageAC(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setUsersTotalCountAC(totalCount));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
