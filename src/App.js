0bef8aa4932c9284493f66525be563012d3a8a8e
Ex-Machin <grinpigrin@gmail.com>
2022-08-16 15:02:44 +0200

    match params

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ const App = () => {
       <div className="app-wrapper-content">
         <Routes>
           <Route path="/dialogs" element={<DialogsContainer />} />
-          <Route path="/profile/*" element={<ProfileContainer />} />
+          <Route path="/profile/:id" element={<ProfileContainer />} />
+          <Route path="/profile" element={<ProfileContainer />} />
           <Route path="/news" element={<News />} />
           <Route path="/music" element={<Music />} />
           <Route path="/settings" element={<Settings />} />
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import { connect } from "react-redux";
 import Profile from "./Profile";
 import { setUserProfile } from "../../redux/profileReducer";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.params.id;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -21,4 +26,24 @@ const mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return (
+      <Component
+        {...props}
+        location={location}
+        navigage={navigate}
+        params={params}
+      />
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, { setUserProfile })(
+  withRouter(ProfileContainer)
+);
